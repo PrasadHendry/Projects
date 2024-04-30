82258ae97746a16ad486ab57d18bcdb4bab8b084
PrasadHendry <prasad.hendry@gmail.com>
2024-04-30 17:43:37 +0530

    none

---
+++ real-estate-app/app/_components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <Image src={"/logo.svg"} width={150} height={150} alt="logo" />
+    </div>
+  );
+}
+
+export default Header;
