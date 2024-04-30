4d18e675543ac5dbb5768c5906358b9c240939c2
PrasadHendry <prasad.hendry@gmail.com>
2024-04-30 16:59:13 +0530

    NOne

---
+++ real-estate-app/app/page.js
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+
+    <div>
+      None!<br></br>
+
+
+      <Button>NOne</Button>
+    </div>
+
+  );
+}
