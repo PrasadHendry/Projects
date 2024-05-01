bdbe5b625ccab6fbaad40f6decff18cf15becb96
PrasadHendry <prasad.hendry@gmail.com>
2024-05-01 11:30:26 +0530

    api loaded

--- real-estate-app/app/(routes)/add-new-listing/page.jsx
+++ real-estate-app/app/(routes)/add-new-listing/page.jsx
@@ -5,8 +5,8 @@ function AddNewListing() {
   return (
     <div className="p-10 flex flex-col  gap-5 items-center justify-center">
       <h2 className="font-bold text-2xl">Add New Listing</h2>
-      <div>
-        <h2 className="text-grey-500">Enter Address for Listing</h2>
+      <div className="w-full max-w-lg">
+        <h2 className="text-grey-500">Enter Address for Listing: </h2>
         <GoogleAddressSearch />
       </div>
     </div>
